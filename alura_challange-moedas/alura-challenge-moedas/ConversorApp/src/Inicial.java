258525829b8fd907e5b5163d3272dd9870fa4fed
Gustavo Silva <nomegusta12345mc@gmail.com>
2025-01-02 21:32:17 -0300

    Add files via upload

---
+++ alura_challange-moedas/alura-challenge-moedas/ConversorApp/src/Inicial.java
@@ -0,0 +1,22 @@
+public class Inicial {
+    public void opcoesDeMoedas(){
+        System.out.println("""
+                ***********************************************************************        
+                
+                                Selecione a Moeda:
+                        
+                                (1) - ARS - Peso argentino
+                                (2) - BOB - Boliviano boliviano
+                                (3) - BRL - Real brasileiro
+                                (4) - CLP - Peso chileno
+                                (5) - COP - Peso colombiano
+                                (6) - USD - Dólar americano
+                
+                                Escolha entre 1 a 6 !
+                                
+                *********************************************************************** 
+                                                    
+                        """);
+        System.out.print("Digite um numero valido entre 1 a 6: ");
+    }
+}
